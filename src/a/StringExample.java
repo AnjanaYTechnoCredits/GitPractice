aecd096b48464f6a8fb57653a2fb0f1d259f3362
Anjana Yadav <anjana.yadav19@gmail.com>
2023-05-23 20:46:58 +0530

    Creating conflict in B

--- src/a/StringExample.java
+++ src/a/StringExample.java
@@ -4,6 +4,6 @@ public class StringExample {
  public static void main(String[] args) {
 	System.out.println("Hello");
 	System.out.println("In Branch A");
-	System.out.println("Creating conflict In Branch A");
+	System.out.println("Creating conflict In Branch B");
  }
 }
